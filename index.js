bfd2132b0bc40619678008b575a28ff13477bba7
Fathir Qisthi <3runrunrun@gmail.com>
2018-10-07 10:02:13 +0700

    change index.js

--- index.js
+++ index.js
@@ -2,26 +2,38 @@
 // Example from quickstart fastify.io
 const fastify = require('fastify')()
 
-const schema = {
-  schema: {
-    response: {
-      200: {
-        type: 'object',
-        properties: {
-          hello: {
-            type: 'string'
-          }
-        }
-      }
-    }
-  }
-}
+/**
+ * default router syntax from fastify one click heroku
+ */
+// const schema = {
+//   schema: {
+//     response: {
+//       200: {
+//         type: 'object',
+//         properties: {
+//           hello: {
+//             type: 'string'
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
 
-fastify
-  .get('/', schema, function (req, reply) {
-    reply
-      .send({ hello: 'world' })
-  })
+// fastify
+//   .get('/', schema, function (req, reply) {
+//     reply
+//       .send({ hello: 'world' })
+//   })
+/**
+ * END OF - default router syntax from fastify one click heroku
+ */
+
+fastify.get('/', (req, res)  => {
+  return {
+    message: 'hello HEROKU!'
+  }
+})
 
 fastify.listen(process.env.PORT || 3000, err => {
   if (err) throw err
